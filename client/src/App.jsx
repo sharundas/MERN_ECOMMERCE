80705add838c02d9e187ccb7e358aad078adaf7e
sharundas <123480220+sharundas@users.noreply.github.com>
2024-03-11 09:39:48 +0530

    frontend-setup completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
+import { Router } from "express";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
 
-import './App.css'
 
 function App() {
-
-
   return (
-    <div className="">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
-  )
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
